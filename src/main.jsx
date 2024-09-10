2fa84060f09bf0c8e22e97deada98d4cf3d2097c
itaezio jardim rodrigues <140777894+itaeziosfw@users.noreply.github.com>
2024-09-09 21:25:13 -0300

    "commit usuarios"

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import  {Login}  from './containers/Login'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
+
 import GlobalStyles from './styles/globalStyles'
+import { ToastContainer } from 'react-toastify'
 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Login/>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    < RouterProvider router={router}/>
     <GlobalStyles/>
-  </StrictMode>,
+    <ToastContainer autoClose={2000} theme='colored'/>
+  </React.StrictMode>,
 )
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import 'react-toastify/dist/ReactToastify.css';
+
 const globalStyles = createGlobalStyle`
 
 *{
@@ -7,10 +9,13 @@ margin:0;
 padding:0;
 box-sizing: border-box;
 outline: none;
+font-family: "Poppins", sans-serif;
+font-weight: 400;
+font-style: normal;
 
 }
 
-button{
+button, a{
     cursor: pointer;
 }
 
